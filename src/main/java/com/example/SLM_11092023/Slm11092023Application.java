be4afed7e8f329cc44535121d295d8b83c10d7b7
Jiri <wi22b004@technikum-wien.at>
2023-09-11 16:53:27 +0200

    Initial commit

---
+++ src/main/java/com/example/SLM_11092023/Slm11092023Application.java
@@ -0,0 +1,13 @@
+package com.example.SLM_11092023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Slm11092023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Slm11092023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SLM_11092023/Slm11092023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SLM_11092023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Slm11092023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
