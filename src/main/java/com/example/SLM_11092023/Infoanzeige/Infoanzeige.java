ff70446d105c0db7cc8832716459d14802000cd3
Jiri <wi22b004@technikum-wien.at>
2023-09-11 18:43:53 +0200

    AM PM Converter hinzugefügt

---
+++ src/main/java/com/example/SLM_11092023/Infoanzeige/Infoanzeige.java
@@ -0,0 +1,12 @@
+package com.example.SLM_11092023.Infoanzeige;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api)")
+public class Infoanzeige {
+  //  @GetMapping("/time/info")
+
+}
